58acf34d7e32d1f4f761467ebf4af876b644005b
sckimosu <sckim7@kookmin.ac.kr>
2017-04-04 14:35:04 +0900

    update

---
+++ hist1.py
@@ -0,0 +1,77 @@
+np.size(minis)
+minis
+minis=np.where(nd<-1)
+plus=np.where(nd<-1)
+np.size(plus)
+plus
+plus=np.where(nd>1)
+np.where(nd>1)
+nd
+plt.plot(nd, '*:')
+plt.figure(5)
+nd=np.random.randn(100)
+plt.plot(ud, ':o')
+plt.plot(ud, 'o')
+plt.figure(4)
+plt.plot(ud)
+plt.figure(3)
+plt.plot(3)
+ud
+plt.figure(ud)
+ud=np.random.uniform(1, -1, 100)
+ud=np.random.uniform(1, -1, 10)
+nd=np.random.randn(10)
+NormalDistribution
+
+NormalDistribution=np.random.randn(FreqReUse, NoUpLink)
+FreqReUse=9; NoUpLink=12
+Dist[0,0]
+np.sqrt((ShiftX[0,0])**2+(ShiftY[0,0])**2)
+np.sqrt((ShiftX[0,:])**2+(ShiftY[0,:])**2)
+plt.grid()
+plt.plot(ShiftX[0,:], ShiftY[0, :], 'r*')
+plt.figure(2)
+ShiftY[0,0]
+ShiftX[0,0]
+plt.figure(1)
+plt.plot(ShiftX, ShiftY, 'bo')
+Dist=np.sqrt(ShiftX**2+ShiftY**2)
+ShiftX=np.array([Cell_x0, Cell_x1, Cell_x2, Cell_x3, Cell_x4, Cell_x5, Cell_x6, Cell_x7, Cell_x8])
+ShiftY=np.array([Cell_y0, Cell_y1, Cell_y2, Cell_y3, Cell_y4, Cell_y5, Cell_y6, Cell_y7, Cell_x8])
+
+plt.figure(1)
+plt.plot(ShiftX, ShiftY, 'bo')
+np.shape(ShiftX)
+ShiftX
+ShiftX=np.array([Cell_x0, Cell_x1, Cell_x2, Cell_x3, Cell_x4, Cell_x5, Cell_x6, Cell_x7, Cell_x8])
+
+np.array(Cell_x0, Cell_x1)
+Cell_x0
+ShiftX=np.array[(Cell_x0, Cell_x1, Cell_x2, Cell_x3, Cell_x4, Cell_x5, Cell_x6, Cell_x7, Cell_x8)]
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+SubX=np.random.uniform(2000, -2000, size=[9, 12])
+SubY=np.random.uniform(2000, -2000, size=[9, 12])
+PlusShift=12000;
+MinusShift=-12000;
+
+Cell_x0=SubX[0,:];
+Cell_y0=SubY[0,:];
+Cell_x1=SubX[1,:];
+Cell_y1=SubY[1,:]+PlusShift;
+Cell_x2=SubX[2,:]+PlusShift;
+Cell_y2=SubY[2,:]+PlusShift;
+Cell_x3=SubX[3,:]+PlusShift;
+Cell_y3=SubY[3,:];
+Cell_x4=SubX[4,:]+PlusShift;
+Cell_y4=SubY[4,:]+MinusShift;
+Cell_x5=SubX[5,:];
+Cell_y5=SubY[5,:]+MinusShift;
+Cell_x6=SubX[6,:]+MinusShift;
+Cell_y6=SubY[6,:]+MinusShift;
+Cell_x7=SubX[7,:]+MinusShift;
+Cell_y7=SubY[7,:];
+Cell_x8=SubX[8,:]+MinusShift;
+Cell_y8=SubY[8,:]+PlusShift;
