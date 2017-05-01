046fe81dde415cefbacf2715961fa95cd60034ce
sckimosu <sckim7@kookmin.ac.kr>
2017-05-01 17:07:03 +0900

    Add histo.py

---
+++ histo.py
@@ -0,0 +1,51 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+s=np.random.uniform(6, 7, 25)
+smin=s.min()
+smax=s.max()
+bin_width=(s.max()-s.min())/4
+first_left=smin
+second_left=smin+bin_width
+third_left=smin+bin_width*2
+fourth_left=smin+bin_width*3
+
+bins_left=np.array([first_left, second_left, third_left,
+fourth_left])
+first_height=np.size(np.where( (s < second_left)
+first_left) ))
+second_height=np.size(np.where( (s < third_left)
+second_left) ))
+third_height=np.size(np.where( (s < fourth_left)
+third_left) ))
+fourth_height=np.size(np.where( (s < fifth_left)
+fourth_left) ))
+& ( s>=
+& ( s>=
+& ( s>=
+& ( s>=
+bins_height=np.array([first_height, second_height,
+third_height, fourth_height])
+
+plt.figure(1)
+plt.subplot(2,1,1)
+plt.bar(bins_left, bins_height, width = bin_width)
+plt.grid()
+
+plt.subplot(2,1,2)
+plt.bar(bins_left, bins_height, width = bin_width-0.05)
+plt.grid()
+
+
+plt.figure(2)
+plt.subplot(2,1,1)
+plt.bar(bins_left, bins_height, width = bin_width)
+plt.grid()
+
+plt.subplot(2,1,2)
+plt.hist(s, bins=4, color=‘m', alpha=0.75)
+plt.grid()
+plt.show()
+
+
+
