394cb90ea20bd8b4b331f0767cacbbe41ddb25f0
sckimosu <sckim7@kookmin.ac.kr>
2017-05-01 10:04:29 +0900

    update

--- hist1.py
+++ hist1.py
@@ -75,3 +75,178 @@ Cell_x7=SubX[7,:]+MinusShift;
 Cell_y7=SubY[7,:];
 Cell_x8=SubX[8,:]+MinusShift;
 Cell_y8=SubY[8,:]+PlusShift;
+
+
+------------------------------
+
+10**(LogNormal[0,:].max()/10)
+
+10**LogNormal[0,:].min()
+
+10**LogNormal[0,:].max()
+
+LogNormal[0,:].min()
+LogNormal[0,:].max()
+LogNormal[0,:]
+LogNormal.min()
+LogNormal.max()
+LogNormal
+plt.plot(LogNormal[0,:]/10)
+plt.figure(11)
+plt.plot(LogNormal[0,:])
+plt.figure(10)
+LogNormal[0,:]/10
+Ps
+mu=0; SD=6
+LogNormal=mu+SD*NormalDistribution
+Ps= 10**(LogNormal[0,:]/10)/Dist[0,:]**4
+ShiftXa=np.array([Cell_x0, Cell_x1, Cell_x2, Cell_x3, Cell_x4, Cell_x5,
+Cell_x6, Cell_x7, Cell_x8])
+ShiftYa=np.array([Cell_y0, Cell_y1, Cell_y2, Cell_y3, Cell_y4, Cell_y5,
+Cell_y6, Cell_y7, Cell_y8])
+Dist=(ShiftXa*ShiftXa+ShiftYa*ShiftYa)
+NormalDistribution=np.random.randn(FreqReUse, NoUpLink)
+x=SubX[1,:]
+y=SubY[1,:]
+Cell_x0=SubX[0,:]
+Cell_y0=SubY[0,:]
+Cell_x1=SubX[1,:]
+Cell_y1=SubY[1,:]+PlusShift
+Cell_x2=SubX[2,:]+PlusShift
+Cell_y2=SubY[2,:]+PlusShift
+Cell_x3=SubX[3,:]+PlusShift
+Cell_y3=SubY[3,:]
+Cell_x4=SubX[4,:]+PlusShift
+Cell_y4=SubY[4,:]+MinusShift
+Cell_x5=SubX[5,:]
+Cell_y5=SubY[5,:]+MinusShift
+Cell_x6=SubX[6,:]+MinusShift
+Cell_y6=SubY[6,:]+MinusShift
+Cell_x7=SubX[7,:]+MinusShift
+Cell_y7=SubY[7,:]
+Cell_x8=SubX[8,:]+MinusShift
+Cell_y8=SubY[8,:]+PlusShift
+SubX=np.random.uniform(NtwSizeA, NtwSizeB, size=[FreqReUse, NoUpLink])
+SubY=np.random.uniform(NtwSizeA, NtwSizeB, size=[FreqReUse, NoUpLink])
+SIR
+SIR=np.zeros((1,NoUpLink))
+import numpy as np
+import matplotlib.pyplot as plt
+FreqReUse=9
+NoUpLink=12
+NtwSizeA=-2000
+NtwSizeB=2000
+PlusShift=12000
+MinusShift=-12000
+No_Iterations=1000
+plt.hist(10**LogNormal/10, 100)
+plt.hist(10**LogNormal, 100)
+plt.figure(3)
+plt.hist(LogNormal/10, 100)
+plt.figure(2)
+plt.hist(LogNormal, 100)
+LogNormal=mu+6*test
+test
+test=np.random.randn(10000)
+test=np.random.randn(10000)
+plt.hist(test, bins=10, color='m')
+plt.hist(SD6, bins=3, normed=True, color='m', alpha=0.75)
+plt.figure(3)
+plt.hist(SD1, bins=3, normed=True, color='r', alpha=0.75)
+who
+SD6=mu+sd6*nd
+SD1=mu+sd1*nd
+mu=0; sd1=1; sd6=6
+nd
+nd=np.random.randn(100)
+
+import numpy as np
+import matplotlib.pyplot as plt
+NormalDistribution=np.random.randn(100)
+10**(LogNormal[0,:].max()/10)
+
+
+10**LogNormal[0,:].min()
+
+
+
+10**LogNormal[0,:].max()
+
+LogNormal[0,:].min()
+LogNormal[0,:].max()
+LogNormal[0,:]
+LogNormal.min()
+LogNormal.max()
+LogNormal
+plt.plot(LogNormal[0,:]/10)
+plt.figure(11)
+plt.plot(LogNormal[0,:])
+plt.figure(10)
+LogNormal[0,:]/10
+Ps
+mu=0; SD=6
+LogNormal=mu+SD*NormalDistribution
+Ps= 10**(LogNormal[0,:]/10)/Dist[0,:]**4
+ShiftXa=np.array([Cell_x0, Cell_x1, Cell_x2, Cell_x3, Cell_x4, Cell_x5,
+Cell_x6, Cell_x7, Cell_x8])
+ShiftYa=np.array([Cell_y0, Cell_y1, Cell_y2, Cell_y3, Cell_y4, Cell_y5,
+Cell_y6, Cell_y7, Cell_y8])
+Dist=(ShiftXa*ShiftXa+ShiftYa*ShiftYa)
+NormalDistribution=np.random.randn(FreqReUse, NoUpLink)
+x=SubX[1,:]
+y=SubY[1,:]
+Cell_x0=SubX[0,:]
+Cell_y0=SubY[0,:]
+Cell_x1=SubX[1,:]
+Cell_y1=SubY[1,:]+PlusShift
+Cell_x2=SubX[2,:]+PlusShift
+Cell_y2=SubY[2,:]+PlusShift
+Cell_x3=SubX[3,:]+PlusShift
+Cell_y3=SubY[3,:]
+Cell_x4=SubX[4,:]+PlusShift
+Cell_y4=SubY[4,:]+MinusShift
+Cell_x5=SubX[5,:]
+Cell_y5=SubY[5,:]+MinusShift
+Cell_x6=SubX[6,:]+MinusShift
+Cell_y6=SubY[6,:]+MinusShift
+Cell_x7=SubX[7,:]+MinusShift
+Cell_y7=SubY[7,:]
+Cell_x8=SubX[8,:]+MinusShift
+Cell_y8=SubY[8,:]+PlusShift
+SubX=np.random.uniform(NtwSizeA, NtwSizeB, size=[FreqReUse, NoUpLink])
+SubY=np.random.uniform(NtwSizeA, NtwSizeB, size=[FreqReUse, NoUpLink])
+SIR
+SIR=np.zeros((1,NoUpLink))
+import numpy as np
+import matplotlib.pyplot as plt
+FreqReUse=9
+NoUpLink=12
+NtwSizeA=-2000
+NtwSizeB=2000
+PlusShift=12000
+MinusShift=-12000
+No_Iterations=1000
+plt.hist(10**LogNormal/10, 100)
+plt.hist(10**LogNormal, 100)
+plt.figure(3)
+plt.hist(LogNormal/10, 100)
+plt.figure(2)
+plt.hist(LogNormal, 100)
+LogNormal=mu+6*test
+test
+test=np.random.randn(10000)
+test=np.random.randn(10000)
+plt.hist(test, bins=10, color='m')
+plt.hist(SD6, bins=3, normed=True, color='m', alpha=0.75)
+plt.figure(3)
+plt.hist(SD1, bins=3, normed=True, color='r', alpha=0.75)
+who
+SD6=mu+sd6*nd
+SD1=mu+sd1*nd
+mu=0; sd1=1; sd6=6
+nd
+nd=np.random.randn(100)
+
+import numpy as np
+import matplotlib.pyplot as plt
+NormalDistribution=np.random.randn(100)
